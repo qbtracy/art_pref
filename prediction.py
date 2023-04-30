a7c2f675a96892f767ea1f0292e12ba77ee38b27
QBT <quinnbtracy@gmail.com>
2023-04-30 00:45:57 +0000

    A few funky prediction.py changes.

--- prediction.py
+++ prediction.py
@@ -1,11 +1,11 @@
-import pandas as pd
-import joblib
 import streamlit as st
+#import pandas as pd
+#import joblib
 
 #Load in our fitted models:
-RAmodel = joblib.load(RAmodel, "RAmodel.joblib")
-LPmodel = joblib.load(LPmodel, "LPmodel.joblib")
-MFmodel = joblib.load(MFmodel, "MFmodel.joblib")
+#RAmodel = joblib.load(RAmodel, "RAmodel.joblib")
+#LPmodel = joblib.load(LPmodel, "LPmodel.joblib")
+#MFmodel = joblib.load(MFmodel, "MFmodel.joblib")
 
 #Now take the user's input and produce a prediction:
 def app():
@@ -14,5 +14,8 @@ def app():
     
     st.markdown('Enter in values into the two fields below. This app will provide a prediction for what type of artwork a person with those characteristics would like.')
     
-    st.number_input('Age:',0,123)
-    st.text_input
+#    st.number_input('Age:',0,123)
+#    st.text_input
+    
+if __name__ == '__main__':
+    app()
