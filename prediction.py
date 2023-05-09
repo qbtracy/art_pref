d1e7abda39f31a3345a57c2b83c5333a90ec93d2
qbtracy <127695643+qbtracy@users.noreply.github.com>
2023-05-08 22:55:57 -0400

    Fixed some formatting and re-added pictures.

--- prediction.py
+++ prediction.py
@@ -16,9 +16,9 @@ def app():
     st.markdown('Choose values from the fields below. This app will provide a prediction for what type of artwork a person with those characteristics would like.')
     
 #    age = st.number_input('Age:',min_value=1,max_value=122)
-    race = st.selectbox('College Major:',options=['Asian','Arab','Black','Indigenous Australian','Native American','White','Other'])
-    gender = st.selectbox('College Major:',options=['Male','Female','Other'])
-    religion = st.selectbox('College Major:',options=['Agnostic','Atheist','Buddhist','Christian (Catholic)',
+    race = st.selectbox('Race:',options=['Asian','Arab','Black','Indigenous Australian','Native American','White','Other'])
+    gender = st.selectbox('Gender:',options=['Male','Female','Other'])
+    religion = st.selectbox('Religion:',options=['Agnostic','Atheist','Buddhist','Christian (Catholic)',
                                                      'Christian (Mormon)','Christian (Protestant)', 'Christian (Other)',
                                                     'Hindu','Jewish','Muslim','Sikh','Other'])
 
@@ -33,21 +33,48 @@ def app():
     LPcat = LPmodel.predict(text_input)[0]
     MFcat = MFmodel.predict(text_input)[0]
     
-
+    if RAcat == 'Realistic':
+        if LPcat == 'Linear':
+            if MFcat == 'Masculine':
+                example = 'ReLnMc.jpg'
+            else:
+                example = 'ReLnFe.jpg'
+        else:
+            if MFcat == 'Masculine':
+                example = 'RePaMc.jpg'
+            else:
+                example = 'RePaFe.jpg'            
+    else:
+        if LPcat == 'Linear':
+            if MFcat == 'Masculine':
+                example = 'AbLnMc.jpg'
+            else:
+                example = 'AbLnFe.jpg'
+        else:
+            if MFcat == 'Masculine':
+                example = 'AbPaMc.jpg'
+            else:
+                example = 'AbPaFe.jpg'  
     
     st.markdown('__We recommend artwork that is:__')
     st.write(RAcat)  
     st.write(LPcat)      
     st.write(MFcat)  
+
+    st.markdown('__An example matching these categories:__')
+
+    st.image(example)#, caption='Example of the above categories.')    
     
     st.markdown('\n\n\n\n\n\n\n')
     
     st.markdown('__What do these categories mean?__')
 
     st.markdown('**Realistic**: artwork which tries to render realistic scenes.')    
-    st.markdown('**Abstract**: artwork which does not try to look realistic.')     
+    st.markdown('**Abstract**: artwork which does not try to look realistic.')
+    st.markdown('\n\n\n\n\n\n\n')
     st.markdown('**Linear**: artwork with clean lines, striving towards photorealism.')
     st.markdown('**Painterly**: artwork with visible brush strokes.')
+    st.markdown('\n\n\n\n\n\n\n')    
     st.markdown('**Masculine**: artwork with content that is stereotypically of interest to males (such as fighting, machines, architecture, etc.).')
     st.markdown('**Feminine**: artwork with content that stereotypically appeals to females (such as women in dresses).')
     
